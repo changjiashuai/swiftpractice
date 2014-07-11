50b7faf85cd4da5c9b86e81fb68fb66738159971
changjiashuai <changjiashuai@gmail.com>
2014-07-11 14:48:41 +0800

    -- init project
    add tab bar

---
+++ swiftpractice/swiftpractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftpractice
+//
+//  Created by CJS on 14-7-11.
+//  Copyright (c) 2014年 CJS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swiftpractice/swiftpracticeTests/swiftpracticeTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftpracticeTests.swift
+//  swiftpracticeTests
+//
+//  Created by CJS on 14-7-11.
+//  Copyright (c) 2014年 CJS. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swiftpracticeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
